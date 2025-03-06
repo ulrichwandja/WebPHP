90a8e3d348fa169d05db48b7c8b761feada941a5
ulrichwandja <nfindajordan@gmail.com>
2025-03-06 14:11:53 +0100

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulaire PHP</title>
+</head>
+<body>
+    <form action="traitement.php" method="post">
+        <label for="nom">Votre nom :</label>
+        <input type="text" id="nom" name="nom">
+        <button type="submit">Envoyer</button>
+    </form>
+</body>
+</html>
 
